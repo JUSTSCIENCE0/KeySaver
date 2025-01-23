75a07a088936a9ed028198ecb4470101ae2640d2
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-01-23 22:55:45 +0700

    add protobuf linux x64 build

--- build_externals.sh
+++ build_externals.sh
@@ -7,6 +7,7 @@ externals_dir=$(pwd)/app/src/main/cpp/external
 externals_src="$externals_dir"/sources
 externals_android_arm64="$externals_dir"/android_arm64-v8a
 externals_android_x86_64="$externals_dir"/android_x86_64
+externals_linux_x86_64="$externals_dir"/linux_x86_64
 
 find_android_ndk() {
     android_ndk_root="$HOME"/Android/Sdk/ndk
@@ -59,19 +60,26 @@ build_openssl() {
 build_protobuf() {
     arch="$1"
     path="$2"
+    platform="$3"
     
     protobuf_build_dir="$externals_src"/protobuf/build
     mkdir "$protobuf_build_dir"
     pushd "$protobuf_build_dir"
-        cmake -G "Unix Makefiles" \
-              -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_ROOT"/build/cmake/android.toolchain.cmake \
-              -DCMAKE_MAKE_PROGRAM="$ANDROID_NDK_ROOT"/prebuilt/linux-x86_64/bin/make \
-              -DANDROID_ABI="$arch" \
-              -DANDROID_PLATFORM=android-24 \
-              -Dprotobuf_BUILD_TESTS=OFF \
-              -Dprotobuf_BUILD_PROTOC_BINARIES=OFF \
-              -DCMAKE_BUILD_TYPE=Release \
-              ..
+        if [[ "$platform" == "android" ]]; then
+            cmake -G "Unix Makefiles" \
+                  -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_ROOT"/build/cmake/android.toolchain.cmake \
+                  -DCMAKE_MAKE_PROGRAM="$ANDROID_NDK_ROOT"/prebuilt/linux-x86_64/bin/make \
+                  -DANDROID_ABI="$arch" \
+                  -DANDROID_PLATFORM=android-24 \
+                  -Dprotobuf_BUILD_TESTS=OFF \
+                  -Dprotobuf_BUILD_PROTOC_BINARIES=OFF \
+                  -DCMAKE_BUILD_TYPE=Release \
+                  ..
+        else
+            cmake -DCMAKE_BUILD_TYPE=Release \
+                  -Dprotobuf_BUILD_TESTS=OFF \
+                  ..
+        fi
           
         cmake --build . -- -j$(nproc)
         cmake --install . --prefix "$path"/protobuf	
@@ -87,7 +95,7 @@ else
 fi
 
 if [ -d "$externals_dir" ]; then
-    rm -r "$externals_dir"
+    rm -rf "$externals_dir"
 fi
 
 mkdir "$externals_dir"
@@ -113,6 +121,7 @@ protobuf_url=https://github.com/protocolbuffers/protobuf.git
 download_sources_git "$protobuf_url" "$protobuf_version"
 
 # build protobuf
-build_protobuf arm64-v8a "$externals_android_arm64"
-build_protobuf x86_64    "$externals_android_x86_64"
+build_protobuf arm64-v8a "$externals_android_arm64"  android
+build_protobuf x86_64    "$externals_android_x86_64" android
+build_protobuf x86_64    "$externals_linux_x86_64"   linux
 
