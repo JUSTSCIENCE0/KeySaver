e89f8b46dc7a732782919662d0e297598dfea380
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-01-28 22:54:55 +0700

    refactor: DBManager draft

---
+++ app/src/main/cpp/db_manager.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#include "db_manager.hpp"
+
+namespace Keysaver {
+} // Keysaver
---
+++ app/src/main/cpp/db_manager.hpp
@@ -0,0 +1,63 @@
+// Copyright (c) 2024, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#pragma once
+
+#include "keysaver/interface.h"
+#include "configuration.pb.h"
+
+#include <array>
+#include <string>
+#include <filesystem>
+#include <cstdint>
+
+namespace Keysaver {
+    class DBManager final {
+    public:
+        // consts
+        static constexpr size_t ENCRYPTION_KEY_SIZE = 32;
+
+        // types
+        using EncryptionKey = std::array<uint8_t, ENCRYPTION_KEY_SIZE>;
+
+        // ctors/dtor
+        explicit DBManager(std::string pathToDB);
+        ~DBManager();
+
+        DBManager(const DBManager&) = delete;
+        DBManager(DBManager&&) = delete;
+        DBManager& operator=(const DBManager&) = delete;
+        DBManager& operator=(DBManager&&) = delete;
+
+        //interface
+        bool IsFileExists() const;
+        bool IsServiceExists(const std::string& serviceName) const;
+        bool IsServiceUrlExists(const std::string& serviceUrl) const;
+        bool IsConfigExists(const std::string& configName) const;
+
+        KeysaverStatus SetEncryptionKey(const EncryptionKey& key);
+        KeysaverStatus Flush() const;
+
+        const KeysaverConfig::DataBase& Get() const { return m_proto_db; }
+        KeysaverConfig::DataBase& Patch() { return m_proto_db; }
+
+    private:
+        // types
+        using uint8_t = std::uint8_t;
+
+        // consts
+        static constexpr auto        DB_NAME = "/database.bin";
+        static constexpr std::string DEFAULT_CONFIG_NAME = "Default";
+
+        // methods
+        KeysaverStatus Read();
+
+        //members
+        KeysaverConfig::DataBase m_proto_db{};
+        std::filesystem::path    m_db_path{};
+        EncryptionKey            m_encryption_key{};
+    };
+} // Keysaver
+
