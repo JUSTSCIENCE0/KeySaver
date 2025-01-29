e564ccc4998654b5310b4e12b2a949ba885c75d4
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-01-29 20:35:22 +0700

    refactor: CryptoProvider draft

--- app/src/main/cpp/crypto_provider.hpp
+++ app/src/main/cpp/crypto_provider.hpp
@@ -5,8 +5,32 @@
 
 #pragma once
 
-namespace Keysaver {
-    class CryptoProvider final {
+#include "keysaver/interface.h"
 
+#include <openssl/types.h>
+
+namespace Keysaver {
+
+    class CryptoProvider final {
+    public:
+        // types
+        enum class HashAlgorithm { SHA3_256, BLAKE2_256 };
+
+        // ctors/dtor
+        CryptoProvider();
+        ~CryptoProvider();
+
+        CryptoProvider(const CryptoProvider&) = delete;
+        CryptoProvider(CryptoProvider&&) = delete;
+        CryptoProvider operator=(const CryptoProvider&) = delete;
+        CryptoProvider operator=(CryptoProvider&&) = delete;
+
+        // interface
+        KeysaverStatus CalculateHash(
+                const void* data, size_t size, HashAlgorithm alg, void* output);
+
+    private:
+        // members
+        EVP_MD_CTX* m_ossl_ctx = nullptr;
     };
 } // Keysaver
