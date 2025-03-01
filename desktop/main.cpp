8544e97f6612f3aeeadb5e98a98399ed65283f88
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-03-01 20:55:36 +0700

    desktop draft

---
+++ desktop/main.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2024, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#include <iostream>
+
+int main() { 
+    std::cout << "Hello, Keysaver!" << std::endl;
+    return 0;
+}
