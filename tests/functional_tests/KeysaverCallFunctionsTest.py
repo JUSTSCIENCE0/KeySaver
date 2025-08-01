04ae0be0413b1119298111ba5b0bf26127e8d3d5
AdaAdaCross <balzhitl@mail.ru>
2025-08-01 18:11:08 +0700

    Added CallFunctions tests, started GeneratePassword test

--- tests/functional_tests/KeysaverCallFunctionsTest.py
+++ tests/functional_tests/KeysaverCallFunctionsTest.py
@@ -2,10 +2,7 @@ import pytest
 import platform
 
 from KeysaverInterface import KeysaverImplementation
-from KeysaverInterface import KeysaverConfiguration
-from KeysaverInterface import KeysaverService as KS
-from KeysaverInterface import ServiseStatus as SS
-from KeysaverInterface import ConfigurationStatus as CS
+from KeysaverInterface import KeysaverStatus as KS
 
 if (platform.system() == 'Windows'):
     path_to_dll = r"..\..\build\bin\RelWithDebInfo\keysaver-core.dll"
@@ -18,9 +15,23 @@ path_to_false_config_dir = r"..\\"
 
 master_password = r"Test123."
 
-service_count = 5
-config_count = 4
-alphabet_count = 2
+conf_list = ['Default',
+             'conf_20_True_False_1_True_3_True_4',
+             'conf_16_True_True_0_True_3_True_4',
+             'conf_20_True_True_0_True_3_True_4']
+
+service_list = ['test_service_Default_configuration',
+                'test_service_1',
+                'test_service_2',
+                'test_service_3',
+                'test_service_0']
+
+alphabet_list = ['Latin (English, US)',
+                 'Кириллица (Русский, Россия)']
+
+service_count = len(service_list)
+config_count = len(conf_list)
+alphabet_count = len(alphabet_list)
 
 @pytest.fixture
 # default keySaver with validate data
@@ -90,11 +101,13 @@ def test_get_services_list(instance):
     result_list = instance.call_function_GetAttributeList('Services')
 
     result.append(len(result_list))
+    result.append(result_list)
     result.append(instance.Close())
     assert result == [
         KS.S_OK.value,  # instance.Init()
         KS.S_OK.value,  # instance.SetMasterPassword()
         service_count,  # len(result_list)
+        service_list,   # result_list
         KS.S_OK.value  # instance.Close()
     ], f"get_services_list  was crashed"
 
@@ -105,13 +118,14 @@ def test_get_config_list(instance):
     result.append(instance.SetMasterPassword())
 
     result_list = instance.call_function_GetAttributeList('Configurations')
-
     result.append(len(result_list))
+    result.append(result_list)
     result.append(instance.Close())
     assert result == [
         KS.S_OK.value,  # instance.Init()
         KS.S_OK.value,  # instance.SetMasterPassword()
         config_count,  # len(result_list)
+        conf_list,      #result_list
         KS.S_OK.value  # instance.Close()
     ], f"get_config_list  was crashed"
 
@@ -122,12 +136,16 @@ def test_get_alphabet_list(instance):
     result.append(instance.SetMasterPassword())
 
     result_list = instance.call_function_GetAttributeList('Alphabets')
-
     result.append(len(result_list))
+    result.append(result_list)
     result.append(instance.Close())
     assert result == [
         KS.S_OK.value,  # instance.Init()
         KS.S_OK.value,  # instance.SetMasterPassword()
         alphabet_count,  # len(result_list)
+        alphabet_list,  # result_list
         KS.S_OK.value  # instance.Close()
     ], f"get_alphabet_list  was crashed"
+
+
+
--- tests/functional_tests/KeysaverGeneratePasswordFunctionalTests.py
+++ tests/functional_tests/KeysaverGeneratePasswordFunctionalTests.py
@@ -1,8 +1,11 @@
 import pytest
 import platform
+
+import KeysaverInterface
 from KeysaverInterface import KeysaverImplementation
 from KeysaverInterface import KeysaverStatus as KS
 from KeysaverInterface import ServiseStatus as SS
+from KeysaverInterface import ConfigurationStatus as CS
 
 if (platform.system() == 'Windows'):
     path_to_dll = r"..\..\build\bin\RelWithDebInfo\keysaver-core.dll"
@@ -17,16 +20,22 @@ master_password = r"Test123."
 false_master_password = r"Test1239999."
 short_master_password = r"Test"
 
+password = b'gM?mWTZei88mE#Nj'
 
 @pytest.fixture
 # default keySaver with validate data
 def instance():
     return KeysaverImplementation(path_to_dll, path_to_config_dir, master_password)
 
+# @pytest.fixture(scope="session", autouse=True)
+# def global_passwords_setup():
+#
+#     # Можно вызвать здесь нужную функцию
+#     # my_setup_function()
 
 # ---------------------------------------------------------------------------------------------#
 
-""" Test of generate_password"""
+""" Test of default generate_password"""
 
 
 def test_generate_password(instance):
@@ -69,3 +78,25 @@ def test_generate_password_invalid_image(instance):
         KS.E_INVALID_ARG.value,  # instance.GeneratePassword(SS.EXIST_SERVICE_NAME_0.value, 10)[0]
         KS.S_OK.value  # instance.Close()
     ], f"GeneratePassword for not exist image was crashed"
+
+
+def test_generate_password_for_ENG_conf(instance):
+    result = []
+    result.append(instance.Init())
+    result.append(instance.SetMasterPassword())
+    service_identity = KeysaverInterface.KeysaverService(url=SS.EXIST_SERVICE_URL_0.value,
+                                                         name=SS.EXIST_SERVICE_NAME_0.value,
+                                                         conf_id=SS.EXIST_CONFIGURATION.value)
+
+    result.append(instance.EditService(SS.EXIST_SERVICE_NAME_0.value, service_identity))
+    result.append(instance.GeneratePassword(SS.EXIST_SERVICE_NAME_0.value, 0))
+    result.append(instance.Close())
+    assert result == [
+        KS.S_OK.value,  # instance.Init()
+        KS.S_OK.value,  # instance.SetMasterPassword()
+        KS.S_OK.value,  # instance.EditService(SS.EXIST_SERVICE_NAME_0.value, service_identity)
+        (KS.S_OK.value, password),
+        KS.S_OK.value  # instance.Close()
+    ], f"GeneratePassword for not exist image was crashed"
+
+
